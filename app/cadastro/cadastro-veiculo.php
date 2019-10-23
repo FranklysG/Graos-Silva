c1cea9bea5c6b4833e0896edb6fde2eae1b72086
henrique_jh <jhenriquemacedo99@gmail.com>
2019-10-22 21:43:26 -0300

    ajustes

---
+++ app/cadastro/cadastro-veiculo.php
@@ -0,0 +1,17 @@
+<form>
+  <div class="form-group">
+    <label for="formGroupExampleInput">Modelo</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
+  </div>
+
+  <div class="form-group">
+    <label for="formGroupExampleInput2">Cor</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="">
+  </div>
+
+  <div class="form-group">
+    <label for="formGroupExampleInput3">Placa</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="">
+  </div>
+
+</form>
