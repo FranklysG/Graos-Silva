350b7099fe080a3227c4180712cab6d28379abe1
henrique_jh <jhenriquemacedo99@gmail.com>
2019-10-30 20:00:19 -0300

    ajustes cadastro veiculo

--- app/cadastro/cadastro-veiculo.php
+++ app/cadastro/cadastro-veiculo.php
@@ -1,1 +1,47 @@
 <h2 id="h3_h">Cadastro Veiculos</h2>
+
+<button type="button" class="btn btn-success butao_veiculo">Novo Veiculo</button>
+
+<table class="table tabela_veiculo">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Modelo</th>
+      <th scope="col">Cor</th>
+      <th scope="col">Placa</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"><img src="https://icon-library.net/images/trash-can-icon-transparent/trash-can-icon-transparent-18.jpg" id="lixo_veiculo"></th>
+      <td id="gol">Atego 2425</td>
+      <td id="gol">Vermelho</td>
+      <td id="gol">PAZ-2019</td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="https://icon-library.net/images/trash-can-icon-transparent/trash-can-icon-transparent-18.jpg" id="lixo_veiculo"></th>
+      <td id="gol">Accelo 815</td>
+      <td id="gol">Branca</td>
+      <td id="gol">HEN-2019</td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="https://icon-library.net/images/trash-can-icon-transparent/trash-can-icon-transparent-18.jpg" id="lixo_veiculo"></th>
+      <td id="gol">VM 260</td>
+      <td id="gol">Cinza</td>
+      <td id="gol">JOSE-2019</td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="https://icon-library.net/images/trash-can-icon-transparent/trash-can-icon-transparent-18.jpg" id="lixo_veiculo"></th>
+      <td id="gol">R440</td>
+      <td id="gol">Preto</td>
+      <td id="gol">BRA-2019</td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="https://icon-library.net/images/trash-can-icon-transparent/trash-can-icon-transparent-18.jpg" id="lixo_veiculo"></th>
+      <td id="gol">FH 460</td>
+      <td id="gol">Azul</td>
+      <td id="gol">HENRIQUE</td>
+    </tr>
+  </tbody>
+</table>
+
