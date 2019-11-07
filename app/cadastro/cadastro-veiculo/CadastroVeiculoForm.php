c95694f009bfeeb0bd859d2341038a521ac27dc6
franklys <tunele095@gmail.com>
2019-11-07 16:14:36 -0300

    novas classes de controle e reorganização das pastas , alterações no banco de dados

--- app/cadastro/cadastro-veiculo/CadastroVeiculoForm.php
+++ app/cadastro/cadastro-veiculo/CadastroVeiculoForm.php
@@ -1,20 +1,58 @@
-<form class="needs-validation" novalidate="" methos="GET">
+<?php
+
+$newVeiculo = new Veiculo;
+$action = new ControlVeiculo;
+$marca = "";
+$cor = "";
+$placa = "";
+$message = "";
+
+$btnSalvar = filter_input(INPUT_POST, "btnSalvar");
+$id = filter_input(INPUT_GET, "id");
+
+if ($id) { }
+if ($btnSalvar) {
+    $newVeiculo->setMarca(filter_input(INPUT_POST, "marca"));
+    $newVeiculo->setCor(filter_input(INPUT_POST, "cor"));
+    $newVeiculo->setPlaca(filter_input(INPUT_POST, "placa"));
+
+    if ($action->add($newVeiculo)) {
+        ?>
+            <div class="alert alert-success">
+                <strong>Success!</strong> Indicates a successful or positive action.
+            </div>
+        <?php
+    } else {
+        ?>
+            <div class="alert alert-danger">
+                <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
+            </div>
+        <?php
+    }
+}
+
+?>
+<form class="needs-validation" method="POST">
     <div class="row">
         <div class="col-sm-4 mb-3">
             <p class="veiculo_title">MARCA
-                <input type="text" class="form-control m-0" name="marca" placeholder="SCANIA" value="" required="">
+                <input type="text" class="form-control m-0" name="marca" placeholder="SCANIA" value="<?php echo $marca ?>" required="">
             </p>
         </div>
         <div class="col-sm-4 mb-3">
             <p class="veiculo_title">COR
-                <input type="text" class="form-control m-0" name="cor" placeholder="BRANCA" value="" required="">
+                <input type="text" class="form-control m-0" name="cor" placeholder="BRANCA" value="<?php echo $cor ?>" required="">
             </p>
         </div>
 
         <div class="col-sm-4 mb-3">
             <p class="veiculo_title">PLACA
-                <input type="text" class="form-control m-0" name="placa" placeholder="XTG 8946" value="" required="">
+                <input type="text" class="form-control m-0" name="placa" placeholder="XTG 8946" value="<?php echo $placa ?>" required="">
             </p>
         </div>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+        <button type="submit" class="btn btn-primary" name="btnSalvar" value="btnSalvar">Salvar</button>
     </div>
 </form>
--- app/model/Load.class.php
+++ app/model/Load.class.php
@@ -7,3 +7,9 @@ include('Fornecedor.class.php');
 include('Motorista.class.php');
 include('Produto.class.php');
 include('Veiculo.class.php');
+
+include(__FILE__.'../../../../lib/controller/ControlCadastroVeiculo.class.php');
+
+include(__FILE__.'../../../../lib/cruds/CrudCadastroVeiculo.class.php');
+
+
