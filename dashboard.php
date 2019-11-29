a6b05ed9bae461f7d97cc1e4f58af86f80e7b34d
franklys <tunele095@gmail.com>
2019-11-29 10:57:49 -0300

    ajustes

--- dashboard.php
+++ dashboard.php
@@ -15,9 +15,9 @@ include('app/model/Load.class.php');
     <script type="text/javascript" src="lib/js/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="lib/js/jquery.maskedinput.js"></script>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="plugins/jquery/jquery.slim.min.js"></script>
+    <script src="plugins/popper/umd/popper.min.js"></script>
+    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
     <title>Grãos Silva!</title>
 
 </head>
