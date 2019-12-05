d179a77634d6ea12abc32a5bd1e886bc265f6c47
marceloo5_5 <dbvmarcelo13@gmail.com>
2019-12-04 21:21:31 -0300

    ajustes

--- triggers.sql
+++ triggers.sql
@@ -2,7 +2,7 @@
 CREATE DEFINER=`root`@`localhost` TRIGGER `graossilva`.`saida_AFTER_INSERT` AFTER INSERT ON `saida` FOR EACH ROW
 BEGIN
 	INSERT INTO entrada (armazem_id, motorista_id, produto_id, fornecedor_id, status_id, chave) VALUES (new.armazem_id, new.motorista_id, new.produto_id, new.cliente_id, 3, new.chave);
-	INSERT INTO graos_silva_log (user_id, tabela, tarefa) VALUES (1, 'SAIDA', 'INSERT');
+	INSERT INTO graos_silva_log (tabela, tarefa) VALUES ('SAIDA', 'INSERT');
 
 END
 
@@ -11,5 +11,5 @@ END
 CREATE DEFINER=`root`@`localhost` TRIGGER `graossilva`.`entrada_AFTER_UPDATE` AFTER UPDATE ON `entrada` FOR EACH ROW
 BEGIN
 	UPDATE saida SET status_id=1 WHERE chave=new.chave;
-	INSERT INTO graos_silva_log (user_id, tabela, tarefa) VALUES (1, 'ENTRADA', 'UPDATE');
+	INSERT INTO graos_silva_log (tabela, tarefa) VALUES ('ENTRADA', 'UPDATE');
 END
